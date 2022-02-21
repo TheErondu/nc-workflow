7b22518b06d49c5201f8dba1ec9305eff9167d7d
Erondu <erone007@gmail.com>
2022-02-21 10:22:40 +0100

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,20 @@
+@extends('layouts.app')
+@section('content')
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-12">
+                <div class="card">
+                    <div class="lottie-full">
+                        <img src="{{ asset('img/screen.jpg') }}"
+                            style="width: 100%; height: 70%;"/>
+
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+@endsection
+@section('javascript')
+
+@endsection
