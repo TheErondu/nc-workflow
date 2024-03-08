7ced38496b901421977178851998d10e33b25028
sysadmin <itsupport@newscentral.ng>
2024-03-08 16:38:29 +0100

    updates

--- resources/views/dashboard/signage/main.blade.php
+++ resources/views/dashboard/signage/main.blade.php
@@ -1,35 +1,12 @@
 
 <script>
+
     // Set the URL to navigate to
     const newURL = '{{ route('signage.show') }}?display={{ $next }}';
 
-    // Function to check internet connectivity
-    function checkInternetConnection() {
-        return navigator.onLine;
-    }
-
-    // Function to retry navigation every 5 seconds
-    function retryNavigation() {
-        setTimeout(function() {
-            if (checkInternetConnection()) {
-                // If online, navigate to the new URL
-                window.location.href = newURL;
-            } else {
-                // If offline, retry after 5 seconds
-                retryNavigation();
-            }
-        }, 5000); // Retry every 5 seconds (5000 milliseconds)
-    }
-
-    // Initial check and start retrying if offline
-    if (checkInternetConnection()) {
-        // If online, navigate to the new URL after 1 minute
-        setTimeout(function() {
-            window.location.href = newURL;
-        }, 60000); // 60000 milliseconds = 1 minute
-    } else {
-        // If offline, start retrying every 5 seconds
-        retryNavigation();
-    }
+    // Delayed navigation after 7 seconds
+    setTimeout(function() {
+        window.location.href = newURL;
+    }, 60000);
+    // 7000 milliseconds = 7 seconds
 </script>
-
