9427460acc426cbfcc676b51ce00e35c6be287c7
sysadmin <itsupport@newscentral.ng>
2025-01-31 09:12:48 +0100

    updates

--- config/filesystems.php
+++ config/filesystems.php
@@ -77,8 +77,7 @@ return [
     */
 
     'links' => [
-        public_path('storage') => storage_path('app/public'),
-        public_path('files') => storage_path('app/public/files'),
+        
     ],
 
 ];
