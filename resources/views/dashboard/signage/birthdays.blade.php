58cad248822bcb615f07d34e9b62cb0fadf2183c
sysadmin <itsupport@newscentral.ng>
2025-09-14 09:23:34 +0100

    updates

--- resources/views/dashboard/signage/birthdays.blade.php
+++ resources/views/dashboard/signage/birthdays.blade.php
@@ -224,7 +224,7 @@
     <section class="cd-slider">
         <ul>
             <li data-color="#00000000">
-                <div class="content" style="background-image:url({{ asset('signage-slides/birthdays/12-09-2025/1.jpg') }}?v=5)">
+                <div class="content" style="background-image:url({{ asset('signage-slides/birthdays/14-09-2025/1.jpg') }}?v=5)">
                 </div>
             </li>
             {{-- <li data-color="#00000000">
