5e30a90bd365f3f54e2d981a2f81ce44d97eb898
sysadmin <itsupport@newscentral.ng>
2025-10-21 08:18:41 +0100

    updates

--- resources/views/dashboard/signage/birthdays.blade.php
+++ resources/views/dashboard/signage/birthdays.blade.php
@@ -227,15 +227,10 @@
             <li data-color="#00000000">
 
                 <div class="content"
-                    style="background-image:url({{ asset('signage-slides/birthdays/18-10-2025/1.jpg') }}?v=5)">
+                    style="background-image:url({{ asset('signage-slides/birthdays/21-10-2025/1.jpg') }}?v=5)">
                 </div>
             </li>
-             <li data-color="#00000000">
 
-                <div class="content"
-                    style="background-image:url({{ asset('signage-slides/birthdays/18-10-2025/2.jpg') }}?v=5)">
-                </div>
-            </li>
             {{-- <li data-color="#00000000">
                 <div class="content"
                     style="background-image:url({{ asset('signage-slides/birthdays/10-10-2025/2.jpg') }}?v=5)">
